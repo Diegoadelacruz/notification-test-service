8af1229cd5790f70451e926c790e0e7617c656ec
Diego de la Cruz <diego.dlacruz@gmail.com>
2023-07-09 13:59:42 -0600

    Initial Commit

---
+++ src/main/java/com/diegodelacruz/notificationtestservice/NotificationTestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.diegodelacruz.notificationtestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationTestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationTestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diegodelacruz/notificationtestservice/NotificationTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegodelacruz.notificationtestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationTestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
