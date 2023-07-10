3bafd311d8ca7c04d3a271d939751345d212c567
Diego de la Cruz <diego.dlacruz@gmail.com>
2023-07-10 11:22:05 -0600

    Add Notification Respository, Service and Controller classes

--- src/main/java/com/diegodelacruz/notificationtestservice/model/Notification.java
+++ src/main/java/com/diegodelacruz/notificationtestservice/model/Notification.java
@@ -1,17 +1,28 @@
 package com.diegodelacruz.notificationtestservice.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
+@Entity
 public class Notification {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idNotification;
 
+    @Column(name = "name", nullable = false, length = 50)
     private String name;
 
+    @Column(name = "description", nullable = false)
     private String description;
 
 }
---
+++ src/main/java/com/diegodelacruz/notificationtestservice/service/INotificationService.java
@@ -0,0 +1,9 @@
+package com.diegodelacruz.notificationtestservice.service;
+
+import com.diegodelacruz.notificationtestservice.model.Notification;
+
+public interface INotificationService {
+
+    public Notification getActualNotification();
+
+}
